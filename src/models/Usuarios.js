4c98dc3026aa03e6fd792b51ab04f303e2170821
IgorPereiraDev <pereirachapa2001@gmail.com>
2020-02-06 16:57:27 -0300

    first commit

---
+++ src/models/Usuarios.js
@@ -0,0 +1,14 @@
+const baseQuery = require('./baseQuery');
+
+
+class Usuarios{
+    insere(usuario){
+        return baseQuery('insert into usuarios set ?', usuario)
+    }
+
+    buscarPorEmail(email){
+        return baseQuery('select * from usuarios where email = ?', email)
+    } 
+}
+
+module.exports = Usuarios;
